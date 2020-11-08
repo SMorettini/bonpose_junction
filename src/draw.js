22439c4e32fb8e071247a1c0d13555654fabb043
ottogin <lukartoil@gmail.com>
2020-11-08 03:27:12 +0100

    FIX: color changed

--- src/draw.js
+++ src/draw.js
@@ -1,7 +1,7 @@
 export function drawPoint(ctx, y, x) {
   ctx.beginPath();
   ctx.arc(x, y, 3, 0, 2 * Math.PI);
-  ctx.fillStyle = 'aqua';
+  ctx.fillStyle = 'chartreuse';
   ctx.fill();
 }
 
@@ -10,6 +10,6 @@ export function drawLine(ctx, y1, x1, y2, x2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.lineWidth = 2;
-  ctx.strokeStyle = 'aqua';
+  ctx.strokeStyle = 'chartreuse';
   ctx.stroke();
 }
